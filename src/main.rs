85d9f812aa47acae2de019393b2360719e99ff8e
erwanriou <rr.1.riou@gmail.com>
2026-04-06 14:47:48 +0200

    add some cards to test formating in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Debug)]
+
+struct Deck {
+    cards: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // LIST OF SUITS & VALUES
+    let suits = ["hearts", "diamonds", "clubs", "spades"];
+    let values = [
+        "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen",
+        "king", "ace",
+    ];
+
+    let mut cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards };
+    println!("Here is your deck: {:#?}", deck);
 }
