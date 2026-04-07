256121ca0573ccfea33af0804764a040f51154ba
erwanriou <rr.1.riou@gmail.com>
2026-04-07 12:43:18 +0200

    Add an implementation class

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,38 @@
 #[derive(Debug)]
 
+// STRUCTURE
 struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    // LIST OF SUITS & VALUES
-    let suits = ["hearts", "diamonds", "clubs", "spades"];
-    let values = [
-        "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen",
-        "king", "ace",
-    ];
+// IMPLEMENTATION (CLASS)
+impl Deck {
+    // ASSOCIATED FUNCTION
+    fn new() -> Self {
+        let suits = ["hearts", "diamonds", "clubs", "spades"];
+        let values = [
+            "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack",
+            "queen", "king", "ace",
+        ];
 
-    let mut cards = vec![];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
+        // IMPLICIT RETURN
+        Self { cards }
     }
 
-    let deck = Deck { cards };
+    // METHOD
+    fn suffle(&self) {}
+}
+
+fn main() {
+    let deck = Deck::new();
+    deck.suffle();
     println!("Here is your deck: {:#?}", deck);
 }
